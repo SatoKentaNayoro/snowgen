3e536fa295472d7b5a6c88a5066e8bccb070b3af
IronC <apehole@163.com>
2023-03-17 20:14:49 +0800

    test_snowflake_id_uniqueness

--- tests/test.rs
+++ tests/test.rs
@@ -12,3 +12,19 @@ fn test_snowflake_generation() {
     let id2 = snowflake.next_id().unwrap();
     assert_ne!(id1, id2);
 }
+
+#[test]
+fn test_snowflake_id_uniqueness() {
+    let snowflake = SnowflakeBuilder::new()
+        .node_id(1)
+        .epoch(Epoch::SystemTime(std::time::UNIX_EPOCH))
+        .build()
+        .unwrap();
+
+    let mut ids = std::collections::HashSet::new();
+    for _ in 0..1000 {
+        ids.insert(snowflake.next_id().unwrap());
+    }
+
+    assert_eq!(ids.len(), 1000);
+}
